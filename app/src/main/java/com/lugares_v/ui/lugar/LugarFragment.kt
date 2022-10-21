215e28bad4eae53e21268a1b9c27279c320ef42a
Roberto Cordoba <robertocordoba@outlook.com>
2022-10-20 18:04:20 -0600

    4a Semana Push 1 Completado sem4

--- app/src/main/java/com/lugares_v/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares_v/ui/lugar/LugarFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lugares_v.R
+import com.lugares_v.adapter.LugarAdapter
 import com.lugares_v.databinding.FragmentLugarBinding
 import com.lugares_v.viewmodel.LugarViewModel
 
@@ -28,6 +30,17 @@ class LugarFragment : Fragment() {
         binding.addLugarFabBt.setOnClickListener {
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
         }
+
+        //Activar el RecylerView con la vista de lugares usando el adapter
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel.getLugares.observe(viewLifecycleOwner) {
+            lugares -> lugarAdapter.setLugares(lugares)
+        }
+
         return binding.root
     }
 
